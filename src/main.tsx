e032b77bb5561840ec599f21d7b36223edf3e149
SoufianeHach <roboter678@googlemail.com>
2025-12-23 13:49:36 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './HamiboomApp' // Falls deine Datei anders heißt, z.B. App.tsx, ändere das hier zu './App'
+import App from './HamiboomApp' 
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
